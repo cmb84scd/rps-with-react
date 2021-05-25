d8ee8a40862d9f4fa948204c3c638d73e8d029af
Catriona Bennett <catriona.bennett@dunelm.com>
2021-05-25 09:58:15 +0100

    Add start game feature test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders start game button', () => {
-  render(<App />);
-  const buttonElement = screen.getByText('Start Game');
-  expect(buttonElement).toBeInTheDocument();
+describe('playing the game', () => {
+  
+  beforeEach(() => render(<App />));
+
+  it('renders start game button', () => {
+    const buttonElement = screen.getByText('Start Game');
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it('game buttons upon clicking start game', () => {
+    fireEvent.click(screen.getByText('Start Game'));
+    const rock = screen.getByText('Rock');
+    const paper = screen.getByText('Paper');
+    const scissors = screen.getByText('Scissors');
+    expect(rock).toBeInTheDocument();
+    expect(paper).toBeInTheDocument();
+    expect(scissors).toBeInTheDocument();
+  });
 });
